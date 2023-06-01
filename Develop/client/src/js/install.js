369918e860b016c89e8180a542c1cb449d4566ae
izzyrkive <119257706+izzyrkive@users.noreply.github.com>
2023-06-01 00:17:46 -0400

    Update install.js
    Minor code changes to event handlers.

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -7,6 +7,51 @@ if (choiceResult.outcome === 'accepted') {
   console.log('PWA installation declined');
 }
 
-butInstall.style.display = 'none';
+const butInstall = document.getElementById('butInstall');
 
-deferredPrompt = null;
+// Add event listener for the click event
+butInstall.addEventListener('click', async () => {
+  try {
+
+    // Prompts the user to install the app
+    if (window.deferredPrompt) {
+      // Shows the installation prompt
+      await window.deferredPrompt.prompt();
+
+      // Wait for the user's response
+      const choiceResult = await window.deferredPrompt.userChoice;
+
+      // Handle the user's choice
+      if (choiceResult.outcome === 'accepted') {
+        console.log('App installed successfully!');
+      } else {
+        console.log('App installation rejected.');
+      }
+      window.deferredPrompt = null;
+    }
+  } catch (error) {
+    console.error('Error handling installation event:', error);
+  }
+});
+
+// Adds event listener for the beforeinstallprompt event
+window.addEventListener('beforeinstallprompt', (event) => {
+  event.preventDefault();
+
+  // Stores the deferred prompt for later use
+  window.deferredPrompt = event;
+
+    // Waits for the user's choice
+    window.deferredPrompt.userChoice
+      .then((choiceResult) => {
+        // Handles the user's choice
+        if (choiceResult.outcome === 'accepted') {
+          console.log('App installed successfully!');
+        } else {
+          console.log('App installation rejected.');
+        }
+
+        window.deferredPrompt = null;
+      });
+  });
+});
